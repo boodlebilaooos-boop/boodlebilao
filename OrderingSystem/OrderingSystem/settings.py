7e006829ea17f44f4a3f566f2cd63c88e84993ab
boodlebilaooos-boop <boodlebilao.oos@gmail.com>
2025-10-23 12:08:23 +0800

    Update settings.py

--- OrderingSystem/OrderingSystem/settings.py
+++ OrderingSystem/OrderingSystem/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "insecure-fallback-key")
 
 DEBUG = os.environ.get("DEBUG", "False").lower() in ["true", "1"]
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "boodlebilao.onrender.com").split(",")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "boodlebilao.onrender.com")
 
 # CSRF Protection for HTTPS
 CSRF_TRUSTED_ORIGINS = [
